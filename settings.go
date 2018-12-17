2ea487a36b0ea1c874ee55a9869c8fcc14b578d8
Greg JEFTIC <ajkula@gmail.com>
2018-12-17 23:24:27 +0100

    added settings file creation and set method, more to do

--- settings.go
+++ settings.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"gopkg.in/yaml.v2"
@@ -11,19 +12,33 @@ type Settings struct {
 	Country string
 }
 
-func noFileError(e error) {}
+func noFileError(err error, UserSettings *Settings) {
+	UserSettings.set("france,fr")
+	if err != nil {
+		fmt.Printf("Error retrieving data: %s\n", err)
+		us, err := yaml.Marshal(&UserSettings)
+		check(err)
+		ioutil.WriteFile("./settings.yml", us, 0644)
+	}
+}
 
 func ReadSettingsFile() {
+	var UserSettings Settings
 	file, err := os.Open("./settings.yml")
-	check(err)
+	noFileError(err, &UserSettings)
 	defer file.Close()
 
-	var UserSettings Settings
 	if file != nil {
 		dec := yaml.NewDecoder(file)
 		dec.Decode(&UserSettings)
 
-		fmt.Println(file)
-		fmt.Println(UserSettings)
+		fmt.Println("UserSettings", UserSettings)
 	}
+}
+
+func (s Settings) set(value string) {
+	s.Country = value
+	us, err := yaml.Marshal(s)
+	check(err)
+	ioutil.WriteFile("./settings.yml", us, 0644)
 }
